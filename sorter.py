076288e042262f31872c173a4747ef44bc40da77
mateo <mateo.marchisone@gmail.com>
2022-06-16 10:35:07 -0300

    get date taken

--- sorter.py
+++ sorter.py
@@ -1,32 +1,25 @@
-import sys
 import os
+
 from PIL import Image
 
-path = "C:/Users/mateo/Desktop/Fotos_Prueba"
-
-number_files = os.listdir(path)
-files_list = []
+directory_path = "./Fotos_Prueba"
 
 
-def get_files_list():
-    for file in number_files:
-        files_list.append(file)
-    return files_list
+def get_files(directory):
+    return os.listdir(directory)
 
 
-def get_date_taken(path):
-    print(Image.open(path)._getexif()[36867])   
-    return Image.open(path)._getexif()[36867]
+def get_number_of_files(files):
+    return len(files)
 
 
-"""
-def get_file_date():
-    for file in files_list:
-        file_path = "C:/Users/mateo/Desktop/Fotos_Prueba/" + file
-        date = os.path.(file_path)
-        print(date)
-    return date
-"""
+def get_date_taken(directory, file):
+    path = f"{directory}/{file}"
+    try:
+        return Image.open(path)._getexif()[36867]
+    except TypeError:
+        pass
 
-get_files_list()
-get_date_taken(path)
+
+for i in get_files(directory_path):
+    print(get_date_taken(directory_path, i))
