4e5d6d32efddc24761f2b69aaaa63773dae9ecd1
mateo <mateo.marchisone@gmail.com>
2022-06-27 11:28:40 -0300

    create year folders

--- sorter.py
+++ sorter.py
@@ -3,6 +3,7 @@ import os
 from PIL import Image
 
 directory_path = "./Fotos_Prueba"
+dates = []
 
 
 def get_files(directory):
@@ -21,5 +22,36 @@ def get_date_taken(directory, file):
         pass
 
 
+def create_main_directory():
+    try:
+        os.mkdir("Fotos_Ordenadas")
+    except FileExistsError:
+        pass
+
+
+def get_years(dates):
+    years = []
+    for date in dates:
+        try:
+            year = date[:4]
+        except TypeError:
+            year = "NONE"
+
+        years.append(year)
+
+    return years
+
+
+def create_years_directories(years):
+    for year in years:
+        try:
+            os.mkdir(f"Fotos_Ordenadas/{year}")
+        except FileExistsError:
+            pass
+
+
 for i in get_files(directory_path):
-    print(get_date_taken(directory_path, i))
+    dates.append(get_date_taken(directory_path, i))
+
+create_main_directory()
+create_years_directories(get_years(dates))
